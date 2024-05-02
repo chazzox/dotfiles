f27a25a48b26cde5b06840675f98e715a6e925b7
charlie.aylott <caylott@outlook.com>
2024-05-02 16:53:06 +0100

    feat: added nvim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,43 @@
+-- CHAZZOX NVIM CONFIG
+
+-- plugins
+require('packer').startup(function(use) 
+  use 'wbthomason/packer.nvim' -- package manager
+  use {
+	'nvim-tree/nvim-tree.lua',
+  	requires = {
+  	  'nvim-tree/nvim-web-devicons', -- optional
+  	},
+  }
+
+  -- languages
+  use 'dag/vim-fish' -- fish syntax 
+  use {'mboughaba/i3config.vim', ft="i3config"} -- i3 config support 
+  use 'wuelnerdotexe/vim-astro'
+
+  use {'nyoom-engineering/oxocarbon.nvim'}  
+end)
+
+
+-- editor stuff
+vim.o.termguicolors = true 
+vim.opt.encoding = "UTF-8" 
+vim.opt.cursorline = true 
+vim.opt.number = true
+vim.opt.mouse = "a" 
+vim.opt.scrolloff = 12
+vim.opt.tabstop = 4 
+vim.opt.shiftwidth = 2 -- 4 character width tabs
+vim.opt.textwidth = 80 
+vim.opt.wrapmargin = 2 -- word wrap
+vim.opt.backup = false 
+vim.opt.writebackup = false -- no annoying backup files
+vim.opt.relativenumber = true
+
+-- configure title string
+vim.opt.title = true 
+
+-- PLUGINS 
+vim.opt.background = "dark" -- set this to dark or light
+vim.cmd.colorscheme "oxocarbon"
+
