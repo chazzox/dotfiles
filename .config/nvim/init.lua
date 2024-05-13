1a5cefadf58ab1372b12a0e22cdf802f75cad2f3
charlie.aylott <charlie.aylott@matillion.com>
2024-05-13 15:13:25 +0100

    chore: alacritty is transparent now! (its awesome)

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -18,7 +18,6 @@ require('packer').startup(function(use)
   use {'nyoom-engineering/oxocarbon.nvim'}  
 end)
 
-
 -- editor stuff
 vim.o.termguicolors = true 
 vim.opt.encoding = "UTF-8" 
@@ -41,3 +40,5 @@ vim.opt.title = true
 vim.opt.background = "dark" -- set this to dark or light
 vim.cmd.colorscheme "oxocarbon"
 
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
