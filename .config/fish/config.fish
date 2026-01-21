f1f8cab584a4c43684b3298e1c4e94c9a7fff809
chazzox <caylott@outlook.com>
2026-01-21 13:01:26 +0000

    feat: split up fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,7 +12,7 @@ function rhul_comp
 end
 
 function progress-commit
-    # makes matillion ticket name i belive?
+    # matches matillion ticket name i belive?
     set -f commit_name (git rev-parse --abbrev-ref HEAD | tr '/' '\n' | sed -n 2p)
     read -p "set_color green; echo -n 'commit description'; set_color normal; echo -n ': '" commit_description
     git add .
@@ -40,17 +40,17 @@ function ls
     command eza $argv --icons
 end
 
-if not set -q TMUX
-    and test "$TERM_PROGRAM" != vscode
-    and test "$TERM_PROGRAM" != "Jetbrains.Fleet"
-    and test "$TERM_PROGRAM" != intellij
-    and test "$TERM_PROGRAM" != "\"intellij\""
-    and test "$TERM_PROGRAM" != OpenLens
-    and test "$TERMINAL_EMULATOR" != JetBrains-JediTerm
-    set -g TMUX ran
-    eval $TMUX
-    ra
-end
+# do not start tmux if terminal is embedded
+#if not set -q TMUX and test "$TERM_PROGRAM" != vscode
+#    and test "$TERM_PROGRAM" != "Jetbrains.Fleet"
+#    and test "$TERM_PROGRAM" != intellij
+#    and test "$TERM_PROGRAM" != "\"intellij\""
+#    and test "$TERM_PROGRAM" != OpenLens
+#    and test "$TERMINAL_EMULATOR" != JetBrains-JediTerm
+#    set -g TMUX ran
+#    eval $TMUX
+#    ra
+#end
 
 set -gx EDITOR nvim
 
@@ -58,22 +58,14 @@ set -Ux PYENV_ROOT $HOME/.pyenv
 
 fish_add_path $PYENV_ROOT/bin
 fish_add_path $HOME/hlint-3.10/
+# completions
 oh-my-posh --init --shell fish --config ~/mytheme.omp.json | source
 pyenv init - | source
 fnm env --use-on-cd | source
-
-# aws completions
-# complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
-
 source (fnm completions --shell fish | psub)
-
-# hold key to repeat system setting on macos
-set -g sysName (uname)
-if test "$sysName" = Darwin
-    defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
-end
-
 glow completion fish | source
+
+#path stuff
 
 bind \cw backward-kill-word
 
@@ -104,30 +96,6 @@ set -l purple 9d7cd8
 set -l cyan 7dcfff
 set -l pink bb9af7
 
-# Syntax Highlighting Colors
-set -g fish_color_normal $foreground
-set -g fish_color_command $cyan
-set -g fish_color_keyword $pink
-set -g fish_color_quote $yellow
-set -g fish_color_redirection $foreground
-set -g fish_color_end $orange
-set -g fish_color_option $pink
-set -g fish_color_error $red
-set -g fish_color_param $purple
-set -g fish_color_comment $comment
-set -g fish_color_selection --background=$selection
-set -g fish_color_search_match --background=$selection
-set -g fish_color_operator $green
-set -g fish_color_escape $pink
-set -g fish_color_autosuggestion $comment
-
-# Completion Pager Colors
-set -g fish_pager_color_progress $comment
-set -g fish_pager_color_prefix $cyan
-set -g fish_pager_color_completion $foreground
-set -g fish_pager_color_description $comment
-set -g fish_pager_color_selected_background --background=$selection
-
 if test -d $HOME/.ghcup
     set --local ghcup $HOME/.ghcup
 
@@ -145,3 +113,11 @@ or fish_add_path --prepend --move --path $HOME/.cabal/bin
 
 contains -- $HOME/.local/bin $fish_user_paths
 or fish_add_path --prepend --move --path $HOME/.local/bin
+
+# hold key to repeat system setting on macos
+set -g sysName (uname)
+if test "$sysName" = Darwin
+    defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+end
+
+source ~/.config/fish/colors.fish
